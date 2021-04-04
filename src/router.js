c8cba94f3b4de8f307a1db8e91245218334c8fee
Essijran <81917144+Essijran@users.noreply.github.com>
2021-04-04 16:40:01 -0400

    Added a seperate file for the routers (#1)
    Co-authored-by: Narjisse Ely <narjisseely@Narjisses-MacBook-Pro.local>

---
+++ src/router.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const route = express.Router()
+
+route.get('/users',(req, res)=>{
+    res.send('Testing the endpoint')
+})
+
+route.get('/user/:id',(req, res)=>{
+    const userId = req.params.id 
+    const {cal} = req.query
+    
+    const msg = cal === 'true' ? 'Sorry we cannot show you the ID': `the userId is : ${userId}` 
+    res.send(msg)
+})
+
+route.post('/book',(req,res)=>{
+    const { name, author } = req.body
+    res.json({
+        name,
+        author
+    })
+}) 
+
+module.exports = route
