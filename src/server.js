9293f2cea8dc5dbc5f9e071f518a3203ab9a0c08
Narjisse Ely <narjisseely@Narjisses-MacBook-Pro.local>
2021-04-04 16:32:38 -0400

    Added a seperate file for the routers

--- src/server.js
+++ src/server.js
@@ -1,29 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const router = require('./router')
 const app = express()
 const port = 4000
 
-app.use(bodyParser())
 
-app.use('/book',(req,res)=>{
-    const { name, author } = req.body
-    res.json({
-        name,
-        author
-    })
-}) 
-app.use('/user/:id',(req, res)=>{
-    const userId = req.params.id 
-    const {cal} = req.query
-    
-    const msg = cal ? 'Sorry we cannot show you the ID': `the userId is : ${userId}` 
-    res.send(msg)
-})
 
-app.use('/', (req, res)=>{
-    res.send('Testing the endpoint')
-})
+app.use(bodyParser.json())
 
+app.use('/api/v1', router)
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`))
 
