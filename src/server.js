9a21ff288dd40c3c6426289cab250aca2942cf09
Narjisse Ely <narjisseely@Narjisses-MacBook-Pro.local>
2021-04-06 19:09:15 -0400

    Created a new branch for the middleware

--- src/server.js
+++ src/server.js
@@ -8,7 +8,12 @@ const port = 4000
 
 app.use(bodyParser.json())
 
-app.use('/api/v1', router)
+app.use((req, res, next)=>{
+    req.age = 19
+    next()
+})
+
+app.use('/api/v1',router)
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`))
 
