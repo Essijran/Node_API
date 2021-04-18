9560d9df517539344eb54a27d3a8eded38f28ace
Narjisse Ely <narjisseely@Narjisses-MacBook-Pro.local>
2021-04-18 12:12:11 -0400

    testing husky

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,6 @@
 const add = (a, b) => {
-  if (Number.isNaN(a) || Number.isNaN(b)) return 'error';
-  return a + b;
-};
+  if (Number.isNaN(a) || Number.isNaN(b)) return 'error'
+  return a + b
+}
 
-module.exports = { add };
+module.exports = { add }
