e24f51967192d0d03216bca3afb7189075de2cbf
Essijran <81917144+Essijran@users.noreply.github.com>
2021-04-18 12:17:44 -0400

    testing husky (#7)
    Co-authored-by: Narjisse Ely <narjisseely@Narjisses-MacBook-Pro.local>

--- src/router.js
+++ src/router.js
@@ -3,30 +3,50 @@ const fetch = require('node-fetch')
 
 const route = express.Router()
 
+route.get('/books', async (req, res) => {
+  const { env } = req.headers
+  const url = env === 'prod' ? 'http://localhost:5000/api/books/4' : 'http://localhost:5000/api/books'
 
-route.get('/books',async (req, res)=>{
-    const url = 'http://localhost:5000/api/books'
-    const {data, status} = await fetch(url)
-    .then( async data => {
-        return {data:await data.json(), status: data.status}
-    })
-    res.status(status).send(data)
+  const { data, status } = await fetch(url)
+    .then(async (d) => ({ data: await d.json(), status: d.status }))
+  res.status(status).send(data)
 })
 
-route.get('/user/:id',(req, res)=>{
-    const userId = req.params.id 
-    const {cal} = req.query
-    
-    const msg = cal === 'true' ? 'Sorry we cannot show you the ID': `the userId is : ${userId}` 
-    res.send(msg)
+route.get('/books/:id', async (req, res) => {
+  const { id } = req.params
+  const url = `http://localhost:5000/api/books/${id}`
+  const { data, status } = await fetch(url)
+    .then(async (d) => ({ data: await d.json(), status: d.status }))
+  res.status(status).send(data)
 })
 
-route.post('/book',(req,res)=>{
-    const { name, author } = req.body
-    res.json({
-        name,
-        author
-    })
-}) 
+route.post('/books', async (req, res) => {
+  const url = 'http://localhost:5000/api/books'
+  const bookObj = { name: req.body.name }
+  const { data, status } = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(bookObj),
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then(async (d) => ({ data: await d.json(), status: d.status }))
+  res.status(status).send(data)
+})
+
+route.put('/books/:id', async (req, res) => {
+  const { id } = req.params
+  const url = `http://localhost:5000/api/books/${id}`
+  const bookObj = { name: req.body.name }
+  const { data, status } = await fetch(url, {
+    method: 'PUT',
+    body: JSON.stringify(bookObj),
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then(async (d) => ({ data: await d.json(), status: d.status }))
+  res.status(status).send(data)
+})
 
 module.exports = route
