69b7aae280b28a1652fbabb55fae781e2b889a9b
Babi_yousuf <vampime@gmail.com>
2021-07-17 19:45:14 -0400

    feat(config): add jest coverage with 100%

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100
+        }
+    },
+    coveragePathIgnorePatterns: [
+        '/src/middleware.js'
+    ]
+}
